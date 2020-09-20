9c9da4e79f85ca40ef05fd94ed7e8c62277c12d9
Christina Poole <codenamedchris@gmail.com>
2020-09-20 22:53:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,65 @@
 # require modules here
 
 def load_library
-  # code goes here
+  {
+      angel => {
+    :english => "O:)",    
+    :japanese => "☜(⌒▽⌒)☞"
+  },
+  angry => {
+    :english => ">:(",
+    :japanese => "ヽ(ｏ`皿′ｏ)ﾉ"
+  },
+  bored => {
+    :english => ":O",
+    :japanese => "(ΘεΘ;)"
+  },
+  confused => {
+    :english => "%)",
+    :japanese => "(゜.゜)"
+  },
+  embarrassed => {
+    :english => ":$", 
+    :japanese => "(#^.^#)"
+  },
+  fish => {
+    :english => "><>",
+    :japanese => ">゜))))彡"
+  },
+  glasses => {
+    :english => "8D",
+    :japanese => "(^0_0^)"
+  },
+  grinning => {
+    :english => "=D",
+    :japanese => "(￣ー￣)"
+  },
+  happy => {
+    :english => ":)",
+    :japanese => "(＾ｖ＾)"
+  },
+  kiss => {
+    :english => ":*",
+    :japanese => "(*^3^)/~☆"
+  },
+  sad => {
+    :english => ":'(",
+    :japanese => "(Ｔ▽Ｔ)"
+  },
+  surprised => {
+    :english => ":o",
+    :japanese => "o_O"
+  },
+  wink => {
+    :english => ";)",
+    :japanese => "(^_-)"
+  }
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(Western_emoticon)
+  
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(Japanese_emoticon)
+  
 end
